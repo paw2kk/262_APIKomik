162732f6e4033528a753c45c4089fa409954f653
paw2k <wndxakk@gmail.com>
2026-07-29 14:15:21 +0700

    fix: handle errors in getAllGenre function and return appropriate response

--- controller/genreController.js
+++ controller/genreController.js
@@ -4,5 +4,8 @@ async function getAllGenre(req, res) {
     try {
         const genre = await db.Genre.findAll();
         res.status(200).json(genre);
+    } catch (err) {
+        console.error('Error fetching genre:', err.message);
+        res.status(500).json({ error: 'Internal Server Error' });
     }
 }
