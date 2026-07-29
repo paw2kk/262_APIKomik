8cf4271be67038fddb89349af0e0d8e376a09682
paw2k <wndxakk@gmail.com>
2026-07-29 14:08:52 +0700

    feat: add id field to Genre model definition in genre.js

--- models/genre.js
+++ models/genre.js
@@ -1,6 +1,10 @@
 module.exports = (sequelize, DataTypes) => {
     const Genre = sequelize.define('Genre', {
-        
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
 
     });
     return Genre;
